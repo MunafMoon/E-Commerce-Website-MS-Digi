de2ce13d3403325c6db844f63ddfdd778f2605fb
MunafMoon <mansurimunaf5@gmail.com>
2026-09-22 13:43:37 +0530

    deploy client to github pages

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,3 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-export default defineConfig({ plugins: [react()], server: { port: 5173 } });
+
+export default defineConfig({
+  base: "/E-Commerce-Website-MS-Digi/",
+  plugins: [react()],
+  server: { port: 5173 }
+});
